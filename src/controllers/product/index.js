ab9d0552519ac183771deb3518d9cd481fd57c5a
unknown <cgonzalez@iiconcepcion.edu.ar>
2021-01-12 01:37:16 -0300

    delete console.logs

--- src/controllers/product/index.js
+++ src/controllers/product/index.js
@@ -1,21 +1,19 @@
-const productModel = require('../../models/product/index')
-const multer = require('multer')
-const { search } = require('../../routers/product')
-const DIR = './public/images/'
-const upload = multer({ dest: './public/images/' }).single('photo')
+const productModel = require("../../models/product/index");
+const multer = require("multer");
+const { search } = require("../../routers/product");
+const DIR = "./public/images/";
+const upload = multer({ dest: "./public/images/" }).single("photo");
 module.exports = {
   getAllProducts: async (req, res) => {
-    const queryName = { name: 'kongo' }
+    const queryName = { name: "kongo" };
     const product = await productModel.find({
-      name: { $regex: '.*' + req.query.search + '.*', $options: 'i' }
-    })
-    console.log(product)
-    res.status(201).send({ product })
+      name: { $regex: ".*" + req.query.search + ".*", $options: "i" },
+    });
+    res.status(201).send({ product });
   },
   getById: async (req, res) => {
-    console.log(req.params.id)
-    const product = await productModel.findById(req.params.id)
-    res.json(product)
+    const product = await productModel.findById(req.params.id);
+    res.json(product);
   },
 
   createProducts: async (req, res, next) => {
@@ -27,15 +25,15 @@ module.exports = {
         price: req.body.price,
         highlith: req.body.highlith,
         images: req.body.images /*,
-        tags:req.body.tags */
-      })
-      const document = await product.save()
-      res.status(201).json(document)
+        tags:req.body.tags */,
+      });
+      const document = await product.save();
+      res.status(201).json(document);
     } catch (e) {
-      console.log(e)
-      res.status(500).send({ message: e.message })
-      res.status(400).send({ message: e.message })
-      res.status(404).send({ message: e.message })
+      console.log(e);
+      res.status(500).send({ message: e.message });
+      res.status(400).send({ message: e.message });
+      res.status(404).send({ message: e.message });
     }
   },
   updateProducts: async (res, req) => {
@@ -43,29 +41,27 @@ module.exports = {
       { _id: req.params.id },
       req.body,
       { multi: fase }
-    )
+    );
   },
   deleteProducts: async (req, res) => {
-    console.log(req.params.id)
-    const product = await productModel.deleteOne({ _id: req.params.id })
+    const product = await productModel.deleteOne({ _id: req.params.id });
   },
 
   upload: async function (req, res, next) {
     try {
       upload(req, res, function (err) {
         if (err) {
-          console.log(err)
-          next()
+          console.log(err);
+          next();
         }
-        console.log(req.file)
         res.status(201).json({
-          status: 'success',
-          message: 'Imagen cargada',
-          data: req.file
-        })
-      })
+          status: "success",
+          message: "Imagen cargada",
+          data: req.file,
+        });
+      });
     } catch (e) {
-      next(e)
+      next(e);
     }
-  }
-}
+  },
+};
--- src/controllers/user/indexWeb.js
+++ src/controllers/user/indexWeb.js
@@ -1,64 +1,60 @@
-const userModel = require('../../models/user/indexWeb')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const PWA = 'pwa'
+const userModel = require("../../models/user/indexWeb");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const PWA = "pwa";
 
 module.exports = {
   login: async (req, res, next) => {
     try {
-      console.log(req.body.email)
+      console.log(req.body.email);
       const { data, error, message } = await userModel.validateUser(
         req.body.email,
         req.body.password
-      )
+      );
       if (!error) {
-        console.log(data)
+        console.log(data);
         const token = jwt.sign({ userId: data }, PWA, {
-          expiresIn: '1h'
-        })
-        res.json({ message: message, token: token })
-        return
+          expiresIn: "1h",
+        });
+        res.json({ message: message, token: token });
+        return;
       }
-      res.json({ message: message })
-      console.log(error, message)
+      res.json({ message: message });
+      console.log(error, message);
     } catch (e) {
-      next(e)
+      next(e);
     }
   },
   getAllUser: async (req, res, next) => {
-    const user = await userModel.find({})
+    const user = await userModel.find({});
     if (res.status(200)) {
-      console.log({ user })
-      res.send({ user })
+      res.send({ user });
     } else {
-      console.log(res.status)
-      res.send(console.log(res.status))
+      res.send(console.log(res.status));
     }
   },
 
   getById: async (req, res, next) => {
-    console.log(req.params.id)
-    const user = await userModel.findById(req.params.id)
-    res.json(user)
+    console.log(req.params.id);
+    const user = await userModel.findById(req.params.id);
+    res.json(user);
   },
   createUserWeb: async (req, res, next) => {
-    console.log(req.body, 'no deberia ir aca')
     try {
-      const users = userModel(({ user, email, password } = req.body))
-      const data = await users.save()
-      res.status(201).send({ data })
+      const users = userModel(({ user, email, password } = req.body));
+      const data = await users.save();
+      res.status(201).send({ data });
     } catch (e) {
-      console.log(e)
-      res.status(200).send({ message: e.message })
+      console.log(e);
+      res.status(200).send({ message: e.message });
     }
   },
   updateProducts: async (req, res, next) => {
     const product = await userModel.update({ _id: req.params.id }, req.body, {
-      multi: fase
-    })
+      multi: fase,
+    });
   },
   deleteUser: async (req, res) => {
-    console.log(req.params.id)
-    const user = await userModel.deleteOne({ _id: req.params.id })
-  }
-}
+    const user = await userModel.deleteOne({ _id: req.params.id });
+  },
+};
